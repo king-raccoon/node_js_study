7fe22af6eeb4fc3ca9dd8a56a85b03ad9806410b
KingPlatypus <dlduswp0408@gmail.com>
2023-05-15 13:20:43 +0900

    make middleware

--- index.js
+++ index.js
@@ -1,48 +1,19 @@
-// //Module
-// // const math = require('./math.js');
+const express = require('express');
+const app = express();
 
-// // const result = math.sum(1, 2);
+function logger(req, res, next){
+    console.log('i am logger'); //클라이언트가 접속 시 서버에 나오는 문구
+    next();
+}
 
-// // console.log(result);
+function logger2(rea, res, next){
+    console.log('i am logger2');
+    next();
+}
 
-// //Synchronous code
-// const fs = require('fs');
+app.use(logger); //미들웨어
+app.use(logger2);
 
-// // const data = fs.readFileSync('./data.txt', 'utf8') 
-// //readFileSync : 파일경로 뒤에 객체 또는 utf8입력하면 해당 경로에 있는 파일에 있는 문자열 출력 가능
-
-// const data = fs.readFile('./data.txt', 'utf8', function(err, data) {
-//     console.log(data); 
-// })
-// //비동기 함수 사용 + 콜백함수 많이 사용함
-
-// //console.log(data); //readFileSync에서 'utf8' 미입력시 버퍼값 출력
-
-//https://nodejs.org/api/synopsis.html
-const http = require('node:http');
-
-const hostname = '127.0.0.1';
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-
-    if(req.url === '/'){
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/plain');
-        res.end('Hello, World!\n');
-    }
-    else if(req.url === '/users'){
-        res.statusCode = 200;
-        res.setHeader('Contenet-Type', 'text/plain');
-        res.end('User list\n');
-    }
-    else {
-        res.statusCode = 404;
-        res.end('Not Found\n');
-    }
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
-//curl -X GET 'localhost:3000' 입력 시 'Hello, World' 출력됨
+app.listen(3000, function(){
+    console.log('Server is running'); //index.js 실행 시 서버에 나오는 문구
+})
